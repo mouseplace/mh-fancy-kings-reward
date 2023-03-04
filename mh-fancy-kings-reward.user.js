cedc1c4d5b4e7778b8099525e0320361b8dba081
Brad Parbs <brad@bradparbs.com>
2023-03-04 17:33:18 -0600

    1.1.0

--- mh-fancy-kings-reward.user.js
+++ mh-fancy-kings-reward.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ Mousehunt - Fancy King's Reward
-// @version      1.0.0
+// @version      1.1.0
 // @description  Makes the King's Reward look nicer and automatically goes back to the page you were on after solving the puzzle.
 // @license      MIT
 // @author       bradp
@@ -9,64 +9,11 @@
 // @icon         https://brrad.com/mouse.png
 // @grant        none
 // @run-at       document-end
+// @require      https://cdn.jsdelivr.net/gh/mouseplace/mousehunt-utils/mousehunt-utils.js
 // ==/UserScript==
 
 ((function () {
 	'use strict';
-
-	/**
-	 * Add styles to the page.
-	 *
-	 * @param {string} styles The styles to add.
-	 */
-	const addStyles = (styles) => {
-		const existingStyles = document.getElementById('mh-mouseplace-custom-styles');
-
-		if (existingStyles) {
-			existingStyles.innerHTML += styles;
-			return;
-		}
-
-		const style = document.createElement('style');
-		style.id = 'mh-mouseplace-custom-styles';
-		style.innerHTML = styles;
-		document.head.appendChild(style);
-	};
-
-	/**
-	 * Do something when ajax requests are completed.
-	 *
-	 * @param {Function} callback    The callback to call when an ajax request is completed.
-	 * @param {string}   url         The url to match. If not provided, all ajax requests will be matched.
-	 * @param {boolean}  skipSuccess Skip the success check.
-	 */
-	const onAjaxRequest = (callback, url = null, skipSuccess = false) => {
-		const req = XMLHttpRequest.prototype.open;
-		XMLHttpRequest.prototype.open = function () {
-			this.addEventListener('load', function () {
-				if (this.responseText) {
-					let response = {};
-					try {
-						response = JSON.parse(this.responseText);
-					} catch (e) {
-						return;
-					}
-
-					if (response.success || skipSuccess) {
-						if (! url) {
-							callback(response);
-							return;
-						}
-
-						if (this.responseURL.indexOf(url) !== -1) {
-							callback(response);
-						}
-					}
-				}
-			});
-			req.apply(this, arguments);
-		};
-	};
 
 	addStyles(`.mousehuntPage-puzzle-form-description, .mousehuntPage-puzzle-form-title, .mousehuntPage-puzzle-form-subtitle {
 		display: none;
