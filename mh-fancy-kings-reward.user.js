64b72bd661e7da53321942a6227f75b1cf63956e
Brad Parbs <brad@bradparbs.com>
2023-04-09 11:01:47 -0500

    Update to 1.2.1

--- mh-fancy-kings-reward.user.js
+++ mh-fancy-kings-reward.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ Mousehunt - Fancy King's Reward
-// @version      1.2.0
+// @version      1.2.1
 // @description  Makes the King's Reward look nicer and automatically goes back to the page you were on after solving the puzzle.
 // @license      MIT
 // @author       bradp
@@ -18,43 +18,37 @@
   addStyles(`.mousehuntPage-puzzle-form-description,
   .mousehuntPage-puzzle-form-title,
   .mousehuntPage-puzzle-form-subtitle,
-  .mousehuntPage-puzzle-form-description,
-  .mousehuntPage-puzzle-form-subtitle,
   .mousehuntPage-puzzle-form-newCode b {
     display: none;
   }
 
   .mousehuntPage-puzzle.active {
+    position: relative;
+    margin-top: 50px;
     background: none;
     border: none;
     box-shadow: none;
-    margin-top: 50px;
   }
 
   .mousehuntPage-puzzle-formContainer {
-    background: none;
-    padding-left: 75px;
-  }
-  .mousehuntPage-puzzle-formContainer {
-    padding: 0;
+    padding: 0 20px 0 75px;
     background: none;
     background-image: url(https://www.mousehuntgame.com/images/ui/newsposts/np_border.png);
     background-repeat: repeat-y;
     background-size: 100%;
-    padding: 0 20px;
   }
 
   .mousehuntPage-puzzle-form-captcha-image-container {
     display: block;
     width: 100%;
-    text-align: center;
     height: 90px;
     margin: 20px auto;
+    text-align: center;
   }
 
   .mousehuntPage-puzzle-form-code-container {
-    width: 300px;
     display: block;
+    width: 300px;
     margin: 0 auto;
     border: none;
     box-shadow: none;
@@ -63,95 +57,85 @@
   .mousehuntPage-puzzle-form-newCode {
     display: block;
     float: none;
+    width: 100%;
     margin: 0;
     text-align: center;
-    width: 100%;
   }
 
   input.mousehuntPage-puzzle-form-code-button {
-    background: linear-gradient(to bottom,#fff600, #f4e830);
-    box-shadow: 0 0 10px 1px #d6d13b inset;
-    font-size: 1.5em;
+    width: 100%;
     padding: 10px 50px;
-    border: 1px solid #000;
-    border-radius: 5px;
+    font-size: 1.5em;
     color: #000;
     text-align: center;
-    width: 100%;
+    background: linear-gradient(to bottom, #fff600, #f4e830);
+    border: 1px solid #000;
+    border-radius: 5px;
+    box-shadow: 0 0 10px 1px #d6d13b inset;
   }
 
   .mousehuntPage-puzzle-form-code-border {
+    width: 100%;
+    height: 90px;
+    margin-bottom: 29px;
     overflow: hidden;
     border-radius: 5px;
-    width: 100%;
-    margin-bottom: 29px;
-    height: 90px;
-  }
-
-  .mousehuntPage-puzzle.active {
-    position: relative;
-    margin-bottom: 150px;
   }
 
   .mousehuntPage-puzzle-form-state.hasPuzzle {
     display: flex;
-  }
-
-  .mousehuntPage-puzzle-form-state.hasPuzzle {
-    display: -ms-grid;
-    display: grid;
     align-items: flex-end;
   }
 
   .mousehuntPage-puzzle-form-title {
-    background: url(https://www.mousehuntgame.com/images/ui/larry_gifts/ribbon.png?asset_cache_version=2) no-repeat;
-    width: 412px;
-    height: 99px;
-    text-align: center;
-    font-size: 16px;
+    position: relative;
+    bottom: 20px;
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 20px auto 0 auto;
+    width: 412px;
+    height: 99px;
+    margin: 20px auto 0;
+    font-size: 16px;
     color: #7d3b0a;
+    text-align: center;
     text-shadow: 1px 1px 1px #e9d5a2;
-    position: relative;
-    bottom: 20px;
+    background: url(https://www.mousehuntgame.com/images/ui/larry_gifts/ribbon.png?asset_cache_version=2) no-repeat;
   }
 
-  .mousehuntPage-puzzle-formContainer.hasPuzzle:before {
-    content: '';
+  .mousehuntPage-puzzle-formContainer.hasPuzzle::before {
     position: absolute;
-    left: 0;
-    right: 0;
-    background-image: url(https://www.mousehuntgame.com/images/ui/newsposts/np_header.png);
-    background-size: 100%;
-    background-repeat: no-repeat;
-    height: 100px;
-    width: 690px;
-    margin: 0 auto;
     top: 0;
+    right: 0;
+    left: 0;
+    width: 690px;
+    height: 100px;
+    margin: 0 auto;
+    content: '';
+    background-image: url(https://www.mousehuntgame.com/images/ui/newsposts/np_header.png);
+    background-repeat: no-repeat;
+    background-size: 100%;
   }
 
-  form.mousehuntPage-puzzle-formContainer.hasPuzzle:after {
-    content: '';
+  form.mousehuntPage-puzzle-formContainer.hasPuzzle::after {
     position: absolute;
-    left: 0;
     right: 0;
-    background-image: url(https://www.mousehuntgame.com/images/ui/newsposts/np_footer.png);
-    background-size: 100%;
-    background-repeat: no-repeat;
-    height: 126px;
+    left: 0;
     width: 690px;
+    height: 126px;
     margin: 0 auto;
+    content: '';
+    background-image: url(https://www.mousehuntgame.com/images/ui/newsposts/np_footer.png);
+    background-repeat: no-repeat;
+    background-size: 100%;
   }
 
   input.mousehuntPage-puzzle-form-code {
-    font-size: 60px;
     height: 90px;
-    background: #e2e2e2;
-    text-align: left;
     padding-left: 10px !important;
+    font-size: 60px;
+    text-align: left;
+    background: #e2e2e2;
   }
 
   .mousehuntPage-puzzle-form-captcha-image-loading {
@@ -161,15 +145,17 @@
   }
 
   .mousehuntPage-puzzle-form-captcha-image img {
+    width: 300px;
     border: 1px solid #666;
     border-radius: 3px;
-    width: 300px;
   }
 
-  input.mousehuntPage-puzzle-form-code-button:hover {
+  input.mousehuntPage-puzzle-form-code-button:hover,
+  input.mousehuntPage-puzzle-form-code-button:focus {
     background: linear-gradient(to bottom, #f4e830, #fff600);
     box-shadow: none;
-  }`);
+  }
+  `);
 
   const init = () => {
     if ('puzzle' !== getCurrentPage()) {
